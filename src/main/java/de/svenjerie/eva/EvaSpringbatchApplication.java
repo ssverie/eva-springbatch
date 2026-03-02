86afa3e222bb7ad01a4b3ff37f226f6141c718a1
ssverie <ssverie@yahoo.de>
2026-03-02 13:38:30 +0100

    Initial Spring Boot base setup

---
+++ src/main/java/de/svenjerie/eva/EvaSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package de.svenjerie.eva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaSpringbatchApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/svenjerie/eva/HealthController.java
@@ -0,0 +1,13 @@
+package de.svenjerie.eva;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "EVA Spring Batch running";
+    }
+}
